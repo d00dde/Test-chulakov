8bbc626ccc95e602ab6fd29748379871c519b335
d00dde <d48564@gmail.com>
2020-05-11 11:57:09 +0300

    deploy

---
+++ precache-manifest.da58012ca81682b3d8676b1eb36aead1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38153b4c2de4832903b3c11e48fe66b6",
+    "url": "/Test-chulakov/index.html"
+  },
+  {
+    "revision": "582200af8837a27481a3",
+    "url": "/Test-chulakov/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ed3d0aece034b74e3e3e",
+    "url": "/Test-chulakov/static/css/main.8b58510b.chunk.css"
+  },
+  {
+    "revision": "582200af8837a27481a3",
+    "url": "/Test-chulakov/static/js/2.36839544.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Test-chulakov/static/js/2.36839544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3d0aece034b74e3e3e",
+    "url": "/Test-chulakov/static/js/main.eb922808.chunk.js"
+  },
+  {
+    "revision": "c416ac22d79d3277896e",
+    "url": "/Test-chulakov/static/js/runtime-main.a6792835.js"
+  }
+]);
